fa8dcdd5cc4117a5c96c5b8ff726800712eca33d
yashantsharma <yashant.sharma@alumni.ashoka.edu.in>
2026-07-20 11:54:31 +0530

    Add floating chat button

--- components/components/AskYashantButton.tsx
+++ components/components/AskYashantButton.tsx
@@ -1,1 +1,16 @@
+"use client";
 
+type Props = {
+  onClick: () => void;
+};
+
+export default function AskYashantButton({ onClick }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      className="fixed bottom-6 right-6 z-50 rounded-full bg-blue-600 hover:bg-blue-700 text-white px-5 py-4 shadow-xl transition-all"
+    >
+      💬 Ask Yashant
+    </button>
+  );
+}
